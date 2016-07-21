9692d0d473596cee74dd3541b73d32094ca6a938
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-07-21 15:05:44 +0200

    extended settings

--- core.lua
+++ core.lua
@@ -35,41 +35,43 @@ function private.SlashHandler(str)
 	str = strupper(str)
 	local parameters = m.Tokenize(str)
 
-	local key = parameters[1]
-	local frame
-	if key == 'PLAYER' then
-		frame = m.player.frame
-	elseif key == 'ENEMY' then
-		frame = m.enemy.frame
+	local frames
+	if parameters[1] == m.player.frame.key then
+		frames = {m.player.frame}
+		tremove(parameters, 1)
+	elseif parameters[1] == m.enemy.frame.key then
+		frames = {m.enemy.frame}
+		tremove(parameters, 1)
 	else
-		return
+		frames = {m.player.frame, m.enemy.frame}
 	end
 
-	if parameters[2] == 'ON' then
-		frame.settings.active = true
-	elseif parameters[2] == 'OFF' then
-		frame.settings.active = false
-	elseif parameters[2] == 'LOCK' then
-		frame.settings.locked = true
-	elseif parameters[2] == 'UNLOCK' then
-		frame.settings.locked = false
-	elseif parameters[2] == 'SIZE' then
-		frame.settings.size = min(20, max(1, tonumber(parameters[3]) or 10))
-	elseif parameters[2] == 'SCALE' then
-		frame.settings.scale = min(2, max(0.5, tonumber(parameters[3]) or 1))
-	elseif parameters[2] == 'CLICK' then
-		frame.settings.clickThrough = not frame.settings.clickThrough
-	elseif parameters[2] == 'IGNORE' then
-		local _, _, ignoreList = strfind(str, '^%s*'..key..'%s+IGNORE%s+(.-)%s*$')
-		frame.settings.ignoreList = ignoreList or ''
-	elseif parameters[2] == 'RESET' then
-		frame:Reset()
-		frame:PlaceFrames()
-	else
-		return
-	end
+	for _, frame in frames do
+		if parameters[1] == 'ON' then
+			frame.settings.active = true
+		elseif parameters[1] == 'OFF' then
+			frame.settings.active = false
+		elseif parameters[1] == 'LOCK' then
+			frame.settings.locked = true
+		elseif parameters[1] == 'UNLOCK' then
+			frame.settings.locked = false
+		elseif parameters[1] == 'SIZE' then
+			frame.settings.size = min(20, max(1, tonumber(parameters[2]) or 10))
+		elseif parameters[1] == 'SCALE' then
+			frame.settings.scale = min(2, max(0.5, tonumber(parameters[2]) or 1))
+		elseif parameters[1] == 'CLICK' then
+			frame.settings.clickThrough = not frame.settings.clickThrough
+		elseif parameters[1] == 'IGNORE' then
+			local _, _, ignoreList = strfind(str, '^%s*'..frame.key..'%s+IGNORE%s+(.-)%s*$')
+			frame.settings.ignoreList = ignoreList or ''
+		elseif parameters[1] == 'RESET' then
+			frame:Reset()
+		else
+			return
+		end
 
-	frame:Initialize()
+		frame:Initialize()
+	end
 end
 
 function private.Tokenize(str)
